1cba28d2384867b2cd904d6ad6e4bdfcfbfff598
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-02 02:05:44 +0530

    Adding code

---
+++ p5-swap.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+print("Before Sawp: \n")
+print (num1)
+print (num2)
+
+temp =  num1
+num1 = num2
+num2 = temp
+
+print("After Swap: \n")
+print (num1)
+print (num2)
+
---
+++ p6-swap.py
@@ -0,0 +1,14 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+print("Before Sawp: \n")
+print (num1)
+print (num2)
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+print("After Swap: \n")
+print (num1)
+print (num2)

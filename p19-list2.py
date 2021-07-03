c268c5771d01b40ddd95a3b673e093418d18174a
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-04 01:24:47 +0530

    Adding code

--- p19-list2.py
+++ p19-list2.py
@@ -1,4 +1,10 @@
 li = [1,2,3,4,5,1,1,1]
+from math import modf
+
+
+from os import name
+
+
 
 #Number 2 is at index 1
 print(li.index(2, 0, 2)) #Start looking from index=0 & ends at index=2
@@ -10,3 +16,29 @@ print('d' in 'dhruvin')
 
 #How many times 1 occure in list
 print(li.count(1))
+
+li1 = ['a', 'q', 'h']
+li1.sort() #Modify the existing list
+print(li1)
+
+print(sorted(li1)) #Creates new copy of list 
+print(li1)
+
+li1.reverse()
+print(li1)
+
+range(1,100)
+print(list(range(1,100)))
+
+s = ' '
+new_s = s.join(['hi', 'my', 'name', 'is', 'Dhruvin'])
+print(new_s)
+
+mod_s = ' '.join(['hi', 'my', 'name', 'is', 'Dhruvin'])
+print(mod_s)
+
+a,b,c, *other = [1,2,3,4,5,6,7,8,9]
+print(a)
+print(b)
+print(c)
+print(other)

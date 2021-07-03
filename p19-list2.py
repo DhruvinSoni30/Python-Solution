1e69f3b7987c3094fe1993bc5ee70cece5491334
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-04 00:43:33 +0530

    Adding code

---
+++ p19-list2.py
@@ -0,0 +1,12 @@
+li = [1,2,3,4,5,1,1,1]
+
+#Number 2 is at index 1
+print(li.index(2, 0, 2)) #Start looking from index=0 & ends at index=2
+
+#Find whether the value is there in list or not
+print(1 in li)
+print(6 in li)
+print('d' in 'dhruvin')
+
+#How many times 1 occure in list
+print(li.count(1))
